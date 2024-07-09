90801bdc327d09b5293be40a206e591eff804afe
awais-ahmad01 <aahmad19376@gmail.com>
2024-07-08 22:34:49 -0700

    Start learning about Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ({initialCount})=>{
+    // state 
+    let [count, setCount] = useState(initialCount);
+
+    const addOne = ()=> {
+        setCount(count+1);
+    }
+
+    const restOne = ()=>{
+        setCount(prevCount => {
+            return prevCount - 1
+        })
+    }
+
+
+
+
+    return(
+        <>
+        
+            <div>
+                Count:{count}
+            </div>
+
+            <button onClick={addOne}>Add One +1</button>
+
+            <button onClick={restOne}>Rest One -1</button>
+
+            <button onClick={()=> setCount(initialCount)}>Reset</button>
+        
+        </>
+    )
 }
 
-export default App;
+export default App
